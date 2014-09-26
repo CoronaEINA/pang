fbf8c9946635fbdc97ce51e99bd0e1e1a251c153
Cristofer <cristofercri@gmail.com>
2014-09-26 09:28:55 +0200

    Añadido const. Puestos listeners.

---
+++ const.lua
@@ -0,0 +1,13 @@
+const = {
+	suelo = {
+		grosor = 20
+	},
+	pared = {
+		grosor = 10
+	},
+	sprite = {
+		escala = 1.5
+	}
+}
+
+return const
--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 local phisics = require("physics")
-
-physics.start( )
-
+local const = require("const")
 local sheetInfo = require("sprites")
 local myImageSheet = graphics.newImageSheet( "sprites.png", sheetInfo:getSheet() )
+
+physics.start( )
 
 local sequence = {
 	{
@@ -30,25 +30,63 @@ local sequence = {
 	}
 }
 
+fondo = display.newImage( "pilar.png", {alpha = .5} )
+fondo.x = display.contentCenterX
+fondo.y = display.contentCenterY
 
+bala = display.newImage( "flecha.png" )
+bala.y = 500
+
+grupo = display.newGroup( )
 local sprite = display.newSprite( myImageSheet , sequence )
-local suelo = display.newLine( 0, display.contentHeight-1, display.contentWidth,  display.contentHeight-1)
-suelo:setColor( 0, 0, 0 )
+local suelo = display.newRect( display.contentCenterX, display.contentHeight - const.suelo.grosor / 2, display.contentWidth + 60, const.suelo.grosor)
+suelo:setFillColor( 0.5, 0.5, 0.5 )
+
+--grupo:insert( sprite )
 
 physics.addBody( sprite, "dynamic", {
-	bounce = 0.001
+	bounce = 0.1
 	} )
-physics.addBody( suelo, "static" )
+
+physics.addBody( suelo, "static", {
+	bounce = 0.1
+	} )
 
 sprite.x = display.contentCenterX
-sprite.y = display.contentHeight - 40
-sprite.xScale = 1.5
-sprite.yScale = 1.5
-sprite:setSequence( "walkForward" )
-sprite:play()
+sprite.xScale = const.sprite.escala
+sprite.yScale = const.sprite.escala
 
-function listener(event)
-	sprite:applyForce( 0, 30, sprite.x, sprite.y - 20 )
+
+function touchListener(event)
+	if (event.phase == "began") then
+		if (event.x > sprite.x + 20) then
+			sprite:setSequence( "walkForward" )
+			transition.to( sprite, {x = display.contentWidth, time = (display.contentWidth - sprite.x) * 5} )	
+			sprite:play()
+		elseif (event.x < sprite.x - 20) then
+			sprite:setSequence( "walkBackward" )
+			transition.to( sprite, {x = 0, time = sprite.x * 5} )		
+			sprite:play()
+		end
+	elseif (event.phase == "ended") then
+		timer.performWithDelay( 100, function()
+			sprite:pause( )
+			sprite:setSequence( "stop" )
+			transition.cancel( sprite )
+		end, 1 )
+	end
 end
 
-Runtime:addEventListener( "tap", listener )
+function disparoListener(event)
+	if (event.phase == "ended") then
+		--bala = display.newImage( "flecha.png" )
+		bala.x = sprite.x
+		bala.y = 500
+		transition.to( bala, {
+			y = -500,
+			time = 1500} )
+	end
+end
+
+Runtime:addEventListener( "touch", touchListener )
+sprite:addEventListener( "tap", disparoListener )
